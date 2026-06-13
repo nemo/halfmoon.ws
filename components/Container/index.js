25160507ee9953b0fd7cf2edadc89116360b38d2
Nima Gardideh <nima@halfmoon.ws>
2026-06-13 13:50:48 -0400

    buy buy old stuff.

--- components/Container/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import styles from './container.module.css'
-
-export default ({ children, className }) => (
-  <div className={`${styles.container} ${className || ''}`}>{children}</div>
-)
